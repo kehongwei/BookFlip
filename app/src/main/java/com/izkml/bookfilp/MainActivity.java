4f7fa9d0218a88f593b888e3dac487018309b5cc
柯红伟 <kehw@izkml.com>
2024-02-27 16:33:34 +0800

    新建项目

---
+++ app/src/main/java/com/izkml/bookfilp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.izkml.bookfilp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.izkml.bookfilp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
